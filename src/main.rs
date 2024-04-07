b26bbe566c4a5853219841e1c6986d02d596bcad
Bartłomiej Kamiński <275430@student.pwr.edu.pl>
2024-04-07 20:17:49 +0200

    second commit - dziala dioda i przycisk

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,41 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_time::Timer;
+use embassy_stm32::gpio::{Level, Output, Speed, Input, Pull};
+use embassy_time::Ticker;
+use embassy_time::Duration;
 use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::task]
+async fn led_blink() {
+    let p = embassy_stm32::init(Default::default());
+    let mut led = Output::new(p.PA5, Level::High, Speed::Low);
+    let mut ticker = Ticker::every(Duration::from_millis(100));
+
+    loop {
+        led.toggle();
+        ticker.next().await;
+    }
+}
+#[embassy_executor::task]
+async fn button(){
+    let p = embassy_stm32::init(Default::default());
+
+    let mut button = ExtiInput::new(p.PC13, p.EXTI13, Pull::Up);
+
+    info!("Press the USER button...");
+
+    loop {
+        button.wait_for_falling_edge().await;
+        info!("Pressed!");
+        button.wait_for_rising_edge().await;
+        info!("Released!");
+    }
+}
 
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
-    let mut led = Output::new(p.PA6, Level::High, Speed::Low);
-
-    loop {
-        led.set_high();
-        Timer::after_millis(300).await;
-        led.set_low();
-        Timer::after_millis(300).await;
-    }
+async fn main(spawner: Spawner) {
+    spawner.spawn(led_blink()).unwrap();
+    spawner.spawn(button()).unwrap();
 }
